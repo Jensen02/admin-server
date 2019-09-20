205dafdb178bf820217c8fd56f7d96d4d36c127e
zhaosongsong <1739416503@qq.com>
2019-09-20 21:48:44 +0800

    add api of category and fix bug

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,17 +4,19 @@
  * @Github: https://github.com/Jensen02
  * @Date: 2019-09-11 01:17:31
  * @LastEditors: Jensen
- * @LastEditTime: 2019-09-17 18:26:14
+ * @LastEditTime: 2019-09-19 18:14:04
  */
-const Router = require('koa-router')
-const userR = require('./user/router')
-const orderR = require('./order/router')
+const Router = require('koa-router');
+const userR = require('./user/router');
+const orderR = require('./order/router');
+const categoryR = require('./category/router');
 
 const router = new Router({
   prefix: '/api/v1'
 })
 
-router.use(userR.routes(), userR.allowedMethods())
-router.use(orderR.routes(), orderR.allowedMethods())
+router.use(userR.routes(), userR.allowedMethods());
+router.use(orderR.routes(), orderR.allowedMethods());
+router.use(categoryR.routes(), categoryR.allowedMethods());
 
 module.exports = router
