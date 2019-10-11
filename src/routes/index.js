7881b866b0044a5dce10e6b66498e1eb22e2e88a
zhaosongsong <1739416503@qq.com>
2019-10-11 21:44:02 +0800

    add train api

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,13 @@
  * @Github: https://github.com/Jensen02
  * @Date: 2019-09-11 01:17:31
  * @LastEditors: Jensen
- * @LastEditTime: 2019-09-19 18:14:04
+ * @LastEditTime: 2019-10-11 13:34:37
  */
 const Router = require('koa-router');
 const userR = require('./user/router');
 const orderR = require('./order/router');
 const categoryR = require('./category/router');
+const trainR = require('./train/router');
 
 const router = new Router({
   prefix: '/api/v1'
@@ -18,5 +19,6 @@ const router = new Router({
 router.use(userR.routes(), userR.allowedMethods());
 router.use(orderR.routes(), orderR.allowedMethods());
 router.use(categoryR.routes(), categoryR.allowedMethods());
+router.use(trainR.routes(), trainR.allowedMethods());
 
 module.exports = router
